5a2efead19e13b9d894e7ebdb77fc16cc334f445
Jonas Åsnes  Sagild <jonassagild@hotmail.com>
2020-06-08 23:17:07 +0200

    handle postbacks for starting and joining a new game

--- src/messenger.js
+++ src/messenger.js
@@ -20,7 +20,9 @@ function callSendAPI(psid, message) {
         params: { access_token: PAGE_ACCESS_TOKEN },
         data: data
     })
-    .catch((error) => {
+    .catch((error) => {message = {
+                "text": constants.ERROR_MSG
+            }
         if (error.response) {
             console.log('PSID: ', psid);
             console.log('Status code: ', error.response.status);
@@ -62,11 +64,42 @@ function handleMessage(sender_psid, received_message) {
  */
 function handlePostback(sender_psid, postback) {
     // TODO: implement
-    // to test, send a message
-    let message = {
-        "text": "PostBack event received"
-    };
-    callSendAPI(sender_psid, message);
+    let message = "";
+
+    // implement join a game and start a new game
+    if (postback.payload == "start_new_game") {
+        // todo
+        message = {
+            "text": "Superb! Please write a name that you wish to use during the game. You may forward the next message" +
+                " to the people that you wish to play with. "
+        };
+        callSendAPI(sender_psid, message);
+        message = {
+            "text": "Hi! Someone invited you to play. Send a message to apprising on facebook to join, and use the code" +
+                " blalbalba to join the game."
+        };
+        callSendAPI(sender_psid, message);
+
+    } else if (postback.payload == "join_a_game") {
+        // todo
+        message = {
+            "text": "Superb! Please write a name that you wish to use during the game."
+        };
+        callSendAPI(sender_psid, message);
+    } else if (postback.payload == "not_right_now") {
+        // todo
+        message = {
+            "text": "We hope to see you again! "
+        };
+        callSendAPI(sender_psid, message);
+
+    } else {
+        message = {
+            "text": constants.ERROR_MSG
+        };
+        callSendAPI(sender_psid, message);
+
+    }
 }
 
 /**
@@ -77,7 +110,7 @@ function createFirstMessage() {
     let buttons = [
         templates.postbackButtonTemplate("Start a new game", "start_new_game"),
         templates.postbackButtonTemplate("Join a game", "join_game"),
-        templates.postbackButtonTemplate("Not right now.", "something")
+        templates.postbackButtonTemplate("Not right now.", "not_right_now")
     ];
     let message = templates.buttonTemplate(constants.WELCOME_MSG, buttons);
     return message;
