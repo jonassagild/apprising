0e42a8979d7581e7ee05b9da958333722edd1546
Jonas Åsnes  Sagild <jonassagild@hotmail.com>
2020-06-08 23:12:57 +0200

    add error message constant

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,5 @@
 module.exports = Object.freeze({
     WELCOME_MSG: 'Hi! Welcome to this game. Do you want to play now?',
     ERROR_POSTBACK: 'Error: do not know what type of webhook_event this is',
+    ERROR_MSG: "Sorry, something went wrong. Please try again later.",
 });
