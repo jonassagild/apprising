771bc69870b1a837c8ef037b4f601e52766bbac2
Jonas Åsnes  Sagild <jonassagild@hotmail.com>
2020-06-08 21:32:31 +0200

    add templates for button and buttons

--- src/templates.js
+++ src/templates.js
@@ -1,2 +1,41 @@
 
 // TODO: make button template
+
+/**
+ *
+ * @param title
+ * @param payload
+ * @returns {{payload: *, type: string, title: *}}
+ */
+function postbackButtonTemplate(title, payload) {
+    return {
+        "type": "postback",
+        "title": title,
+        "payload": payload,
+    }
+}
+
+/**
+ *
+ * @param text
+ * @param buttons
+ * @returns {{attachment: {payload: {buttons: *, template_type: string, text: *}, type: string}}}
+ */
+function buttonTemplate(text, buttons) {
+    return {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "button",
+                "text": text,
+                "buttons": buttons
+            }
+        }
+    }
+}
+
+module.exports = {
+    postbackButtonTemplate: postbackButtonTemplate,
+    buttonTemplate: buttonTemplate,
+
+};
